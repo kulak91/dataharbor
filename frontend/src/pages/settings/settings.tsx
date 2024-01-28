16e244184c2bafd6c22ef7d7f8d1c00ace27963f
Dmytro Kulakov <kulakovdmitr@gmail.com>
2024-01-28 14:24:48 +0200

    dat-21: - remove dev button

--- frontend/src/pages/settings/settings.tsx
+++ frontend/src/pages/settings/settings.tsx
@@ -3,22 +3,9 @@ import type { FC } from 'react';
 import styles from './styles.module.scss';
 
 const Settings: FC = () => {
-  const handleClick = async (): Promise<void> => {
-    try {
-      const res = await fetch('http://localhost:3001/api/v1/users/test', {
-        method: 'GET',
-      });
-      console.log('res', await res.json());
-    } catch (error) {
-      console.log('err', error);
-    }
-  };
-
   return (
     <div>
       <h1 className={styles['title']}>Settings Screen </h1>
-
-      <button onClick={handleClick}>Click</button>
     </div>
   );
 };
